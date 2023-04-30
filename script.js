0133c9bec946baa5b7bfd83a507b2cd2a26d8e90
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-04-30 09:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your JS code here. If required.
-function meta() {
+let enterBtn = document.getElementById('enterBtn')
+
+enterBtn.document.querySelector('click',function {
 	let para = document.getElementById('para')
 	para.innerText = "Entered Metaverse"
-}
+})
